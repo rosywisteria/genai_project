65bff3cd8e2d6d9f1e976be4a62ed429b487146f
emily wei <141193425+rosywisteria@users.noreply.github.com>
2025-09-15 16:37:40 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+
+app.use(cors({
+  origin: ["http://localhost:3000"],
+//   origin: ["https://chatting-3tub.onrender.com"],
+  methods: ["GET","POST"],
+  credentials: true
+}));
 
 app.use(express.json());
 
